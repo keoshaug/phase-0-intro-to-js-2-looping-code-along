6c1a39fde5b20de49c163e07187360b348f2bd52
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-03-08 11:29:01 -0500

    creates looping code.md

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+
+function writeCards( names, value ) {
+    const messageNames = []
+   for ( let i = 0; i < names.length; i++ ) {
+      messageNames.push( `Thank you, ${names[i]}, for the wonderful ${value} gift!` )
+
+    }
+    return messageNames
+  }
+  
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+function countDown(number) {
+    let value = number; 
+    while (value >= 0) {
+      console.log(value --);
+    }
+    return number;
+  }
+  
+
+  
